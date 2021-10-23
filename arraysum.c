222d28febf48ef09525e745e98acf89a2eff83a4
Manasasamavedam <92861533+Manasasamavedam@users.noreply.github.com>
2021-10-23 23:10:03 +0530

    Update arraysum.c

--- arraysum.c
+++ arraysum.c
@@ -2,7 +2,7 @@
 int main()    
 {    
  int n1=0,n2=1,n3,i,n;    
- printf("Enter the number of elements:");    
+ printf("enter the array of elements:\n");    
  scanf("%d",&n);    
  printf("\n%d %d",n1,n2);    
  for(i=2;i<n;++i)   
